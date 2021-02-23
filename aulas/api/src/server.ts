1b78b326899ef22682b6779355cb82c426ca71c1
usuario <rodolfo.ds@aluno.ifsc.edu.br>
2021-02-23 10:46:24 -0300

    second

--- aulas/api/src/server.ts
+++ aulas/api/src/server.ts
@@ -1,13 +1,16 @@
+import 'reflect-metadata'
 import express, { response } from 'express';
 
 const app = express();
 
+import './database'
+
 app.get("/", (resquest, response) => {
-    return response.send("Hello Rodolfo")
+  return response.send("Hello Rodolfo")
 })
 
 app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso!" })
+  return response.json({ message: "Os dados foram salvos com sucesso!" })
 })
 
 app.listen(3333, () => console.log("server is runing"));
